d5a179507ff3082a4ae104added3514260a167e1
Luiz Silveira <l.filiphis@hotmail.com>
2023-01-12 22:25:05 -0300

    Finalizando estudo sobre API do curso codeboost. Desenvolvido uma consulta simples a uma API do Ricky And Morty

--- deploy.sh
+++ deploy.sh
@@ -25,6 +25,6 @@ git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
-git push -f git@github.com:filiphis/estudando-consumo-a-api.git consumindo-api-curso-codeboost:gh-pages
+git push -f git@github.com:filiphis/estudando-consumo-a-api.git main:gh-pages
 
 cd -
